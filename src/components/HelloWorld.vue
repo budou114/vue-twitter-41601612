edbd3aebb21e99f9eca89612dbdc7b23d2f11e7d
budou114 <58349030+budou114@users.noreply.github.com>
2024-03-02 11:53:54 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const tweets = ref([
+  "Tweet1",
+  "Tweet2",
+  "Tweet3",
+]);
+
+</script>
+
+<template>
+  <h1>Twitter</h1>
+  <ul>
+    <li v-for="tweet in tweets" :key="tweet">{{ tweet }}</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-top: 1rem;
+}
+</style>
