f7c6a36a69821474c894a7e45ff4ae7681c06a4c
budou114 <58349030+budou114@users.noreply.github.com>
2024-03-02 12:26:13 +0900

    46. リストから要素を削除する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,61 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const tweets = ref([
-  "Tweet1",
-  "Tweet2",
-  "Tweet3",
+type Tweet = {
+  id: string;
+  description: string;
+}
+
+const tweets = ref<Tweet[]>([
+  {
+    id: "1",
+    description: "Hello World1",
+  },
+  {
+    id: "2",
+    description: "Hello World2",
+  }
 ]);
 
-const tweet = ref("");
+const tweet = ref<Tweet>({
+  id: "",
+  description: ""
+});
 
 const onSubmit = () => {
-  tweets.value.push(tweet.value);
-  tweet.value = "";
+  tweets.value = [
+    ...tweets.value,
+    {
+      id: Math.random.toString(),
+      description: tweet.value.description,
+    }
+  ];
+
+  tweet.value = {
+    id: "",
+    description: ""
+  };
 };
+
+const onDelete = (id: string) => {
+  tweets.value = tweets.value.filter((tweet) => tweet.id !== id);
+}
 
 </script>
 
 <template>
   <h1>Twitter</h1>
   <form @submit.prevent="onSubmit()">
-    <input type="text" v-model="tweet" />
+    <input type="text" v-model="tweet.description" />
     <button type="submit">Tweet</button>
   </form>
   <ul>
-    <li v-for="tweet in tweets" :key="tweet">{{ tweet }}</li>
+    <li v-for="tweet in tweets" :key="tweet.id">
+      <div>
+        <span>{{ tweet.description }}</span>
+        <button @click="() => onDelete(tweet.id)">Delete</button>
+      </div>
+    </li>
   </ul>
 </template>
 
